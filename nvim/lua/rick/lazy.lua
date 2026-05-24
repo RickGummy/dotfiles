a88437c859b8759243e82008d68af9657b047c01
RickGummy <xiufupeng@gmail.com>
2026-05-24 13:35:24 -0400

    IM TOO LAZY TO WRITE ONE SO THIS IS AN AUTO COMMIT, LOL

--- nvim/lua/rick/lazy.lua
+++ nvim/lua/rick/lazy.lua
@@ -270,4 +270,19 @@ require("lazy").setup({
             })
         end,
     },
+
+    -- Statusline
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    theme = "auto",
+                    section_separators = "",
+                    component_separators = "",
+                },
+            })
+        end,
+    },
 })
